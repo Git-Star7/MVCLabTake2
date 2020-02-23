6ab1ee1da77889d10f484912343e0692bb542d52
Michael <Michael@acdmail.com>
2020-02-23 14:10:43 -0500

    first commit

---
+++ MVC Lab Take 2/Models/Tripometer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Lab_Take_2.Models
+{
+    public class Tripometer
+    {
+        public double MPG { get; set; }
+        public double Gallons { get; set; }
+        public double Trip { get; set; }
+
+    }
+}
