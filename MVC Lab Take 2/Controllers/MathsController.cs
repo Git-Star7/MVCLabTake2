0fe603f9b5f7bd56dcfa6221c61efc17fa5af2af
Michael <Michael@acdmail.com>
2020-02-24 22:17:40 -0500

    added Add and Divide

---
+++ MVC Lab Take 2/Controllers/MathsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Lab_Take_2.Models;
+
+namespace MVC_Lab_Take_2.Controllers
+{
+    public class MathsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddNumbers()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddNumbers(int num1, int num2)
+        {
+            string sum = Maths.Add(num1, num2);
+            return View("Results", sum);
+        }
+        [HttpGet]
+        public IActionResult DivideNumbers()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DivideNumbers(int num1, int num2)
+        {
+            string result = Maths.Divide(num1, num2);
+            return View("Results", result);
+        }
+        public IActionResult Results()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ MVC Lab Take 2/Models/Maths.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Lab_Take_2.Models
+{
+    public class Maths
+    {
+        public static string Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result.ToString();
+        }
+        public static string Divide(int num1, int num2)
+        {
+            double divNum = num1 / num2;
+            double divRemainder = num1 % num2;
+
+            return $"{divNum} with a ramainder of {divRemainder}";
+        }
+    }
+}
